5c91c718480bbbc698589aeafe13e685ec6b35ea
Abdallahelbaz <abdallah.elbaz19@gmail.com>
2024-07-01 14:03:10 +0200

    uncomment controller

--- src/main/java/com/example/Cafateria/Controllers/UserController.java
+++ src/main/java/com/example/Cafateria/Controllers/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 //
-//@Controller
+@Controller
 //@RequestMapping("api/users")
 //@AllArgsConstructor
 public class UserController {
